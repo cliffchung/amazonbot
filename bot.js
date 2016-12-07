e2fca5e5facbaa4bf179d9e242283488759ae732
Cliff Chung <cliffchung@Cliffs-MacBook-Pro.local>
2016-12-06 22:36:27 -0800

    Enhancing slashcommand
    It now uses delayed responses so that we can replace the initial
    message.  Unfortunately can't replace it with 0 text.

--- bot.js
+++ bot.js
@@ -1,5 +1,10 @@
+'use strict';
+
 const BOT_BUILDER = require('claudia-bot-builder');
 const SLACK_TEMPLATE = BOT_BUILDER.slackTemplate;
+const SLACK_DELAYED_REPLY = BOT_BUILDER.slackDelayedReply;
+const aws = require('aws-sdk');
+const lambda = new aws.Lambda();
 
 const REF_TAG_CLIFF = "cliffchung-20";
 const REF_TAG_RICHARD = "tridium-20";
@@ -17,6 +22,8 @@ function getRefTagToUse(message) {
     var splitString = message.text.split(" ");
 
     var refTagToUse = "";
+
+    console.log("Split string " + splitString);
 
     if (splitString.length > 1) {
         var refTagArgument = splitString[1].toLowerCase();
@@ -43,7 +50,7 @@ function getRefTagToUse(message) {
     return refTagToUse;
 }
 
-module.exports = BOT_BUILDER(function (message) {
+const api = BOT_BUILDER((message, apiRequest) => {
     // but first, let's make sure the token matches!
     if (message.originalRequest.token !== VERIFICATION_TOKEN) {
         return;
@@ -55,23 +62,67 @@ module.exports = BOT_BUILDER(function (message) {
     }
 
     if (AMZN_LINK_REGEX.test(message.text)) {
-        // get the 2nd match group
-        var strippedLink = AMZN_LINK_REGEX.exec(message.text)[2];
+        return new Promise((resolve, reject) => {
+            lambda.invoke({
+                FunctionName: apiRequest.lambdaContext.functionName,
+                InvocationType: 'Event',
+                Payload: JSON.stringify({
+                    slackEvent: message
+                }),
+                Qualifier: apiRequest.lambdaContext.functionVersion
+            }, (err, done) => {
+                if (err) console.log(err, err.stack);
 
-        if (strippedLink.charAt(strippedLink.length - 1) != '\/') {
-            strippedLink += "\/";
-        }
-
-        var refTagToUse = getRefTagToUse(message);
-        strippedLink = strippedLink + REF_TAG_PREPEND + refTagToUse;
-
-        var returnMessage = new SLACK_TEMPLATE(message.originalRequest.user_name
-                + " requested a link: " + strippedLink);
-        return returnMessage
-            .replaceOriginal(true) // replace the original message with the updated one
-            .channelMessage(true) // announce it to the channel
-            .get();
+                resolve();
+            });
+        })
+        .then(() => {
+            return new SLACK_TEMPLATE("")
+                .replaceOriginal(true)
+                .addAttachment()
+                .addPretext("Creating link...")
+                .get();
+        })
+        .catch(() => {
+            return "Could not create Amazon link :("
+        });
     } else {
         return USAGE;
     }
 });
+
+
+api.intercept((event) => {
+    if (!event.slackEvent) { // if this is a normal web request, let it run
+        return event;
+    }
+
+    const message = event.slackEvent;
+
+    // get the 2nd match group
+    var strippedLink = AMZN_LINK_REGEX.exec(message.text)[2];
+
+    if (strippedLink.charAt(strippedLink.length - 1) != '\/') {
+        strippedLink += "\/";
+    }
+
+    var refTagToUse = getRefTagToUse(message);
+    console.log(JSON.stringify(message));
+    console.log("RefTag: " + refTagToUse)
+
+        strippedLink = strippedLink + REF_TAG_PREPEND + refTagToUse;
+
+    var returnMessage = new SLACK_TEMPLATE("")
+        .channelMessage(true) // announce it to the channel
+        .addAttachment()
+        .addText(strippedLink)
+        .addPretext(
+                message.originalRequest.user_name
+                + " requested a link")
+        .addColor("#7CD197")
+        .get();
+    return SLACK_DELAYED_REPLY(message, returnMessage)
+        .then(() => false); // prevent normal execution
+});
+
+module.exports = api;
